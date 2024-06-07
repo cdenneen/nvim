f5134a1ba9d13cd3c06bb0cc7995e163e3625938
Chris Denneen <cdenneen@gmail.com>
2024-06-07 09:36:36 -0400

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 require("lazy").setup({
 	spec = {
-		"LazyVim/LazyVim",
-		import = "lazyvim.plugins",
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 	},
 })
