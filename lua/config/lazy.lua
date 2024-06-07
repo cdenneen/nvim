fdbc03e040eb796fe1c51c2292015f2e5605d1b8
Chris Denneen <cdenneen@gmail.com>
2024-06-07 11:32:08 -0400

    Update lazy.lua
    Update lockfile

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -62,6 +62,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  lockfile = vim.fn.stdpath('data') .. '/lazy/lazy-lock.json',
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
